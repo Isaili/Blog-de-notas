92b08b6d3544f1a1c42db73c3b3b4a2ae18666a9
proyecto-clinica <isai123abel@gmail.com>
2025-02-04 18:23:23 -0600

    Blog de Notas 1.2

--- index.php
+++ index.php
@@ -61,7 +61,7 @@ if (isset($_GET['edit']) && isset($_GET['index'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistema de Notas Mejorado</title>
+    <title>Sistema de Notas</title>
     <style>
         body {
             font-family: Arial, sans-serif;
